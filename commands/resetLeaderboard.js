daf843237d098473b823d02a480c6d4debf10a3d
ZIKOpl <mathisalexandre89@gmail.com>
2025-10-01 07:58:26 +0200

    Update and rename staffdeletevouch.js to resetLeaderboard.js

---
+++ commands/resetLeaderboard.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+const updateLeaderboard = require("../utils/leaderboard");
+
+const { SlashCommandBuilder } = require('discord.js');
+const { saveVouches, VOUCH_FILE } = require('../utils/leaderboard');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('resetleaderboard')
+        .setDescription('Réinitialise toutes les vouches (Admin seulement)'),
+    async execute(interaction, client) {
+        if (!interaction.member.permissions.has('Administrator'))
+            return interaction.reply({ content: 'Tu n’as pas la permission.', ephemeral: true });
+
+        saveVouches({});
+        await interaction.reply({ content: 'Leaderboard réinitialisé ! ✅', ephemeral: true });
+    }
+};
